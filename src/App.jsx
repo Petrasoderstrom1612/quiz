b51e40793c4b4798e204823b01a8797f190ae6c8
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-07-24 14:06:02 +0200

    Starter screen with the onclick function done, just background to be added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import React from 'react'
+import Intro from './components/Intro'
 
 function App() {
-  const [questions, setQuestions] = React.useState({
-    id: 0,
-    question: "",
-    correctAnswer: "",
-    answers: [],
-  })
+  const [startScreen, setStartScreen] = React.useState(true)
+  const [questions, setQuestions] = React.useState(null)
+  const [answer, setAnswer] = React.useState(null)
 
+  
   React.useEffect(()=>{
     fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      setQuestions(data.results)
+      console.log("questions",questions)
+    })
   },[])
 
+  const startQuiz = () => {
+    setStartScreen(prevStatus => !prevStatus)
+  }
+  console.log(startScreen)
+
   return (
-    <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      {startScreen && (<Intro startQuiz={startQuiz}/>)}
+      {!startScreen && (
+        <>
+        <p>questions section</p>
+        </>
+      )}
+    </main>
   )
 }
 
---
+++ src/components/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Intro = ({startQuiz}) => {
+  return (
+    <div>
+      <h1>Quizzical</h1>
+      <p>Answer 5 questions to check how much common knowledge you have.</p>
+      <button className='starter-btn' onClick={startQuiz}>Start quiz</button>
+    </div>
+  )
+}
+
+export default Intro
