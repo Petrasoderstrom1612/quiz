0cbf6d4d634f0185c8c5448a83a0156b9f5dce8e
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-07-24 15:55:53 +0200

    added decode

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import Intro from './components/Intro'
 import BackgroundWrapper from './components/BackgroundWrapper'
+import {decode} from 'html-entities';
 
 function App() {
-  const [startScreen, setStartScreen] = React.useState(true)
+  const [startScreen, setStartScreen] = React.useState(false)
   const [questions, setQuestions] = React.useState(null)
   const [answer, setAnswer] = React.useState(null)
 
+  const startQuiz = () => {
+    setStartScreen(prevStatus => !prevStatus)
+  }
+  console.log(startScreen)
   
   React.useEffect(()=>{
     fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
     .then(res => res.json())
     .then(data => {
       setQuestions(data.results)
-      console.log("questions",questions)
     })
   },[])
-
-  const startQuiz = () => {
-    setStartScreen(prevStatus => !prevStatus)
-  }
-  console.log(startScreen)
+  console.log("questions",questions)
+  
+  const questionSection = questions
+  ? questions.map(oneQuestion => (
+      <h2 key={oneQuestion.question}>{decode(oneQuestion.question)}</h2>
+    ))
+  : null
 
   return (
     <main>
@@ -28,7 +34,7 @@ function App() {
       {startScreen && (<Intro startQuiz={startQuiz}/>)}
       {!startScreen && (
         <>
-        <p>questions section</p>
+        {questionSection}
         </>
       )}
     </main>
