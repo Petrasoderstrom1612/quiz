df31aab316a8289f58a6999632b87cf10211a8d8
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-07-25 08:50:27 +0200

    added all answers by using splice with random Index to add correct Answer

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,34 @@ function App() {
   console.log(startScreen)
   
   React.useEffect(()=>{
+    const timeoutId = setTimeout(() => {
     fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
     .then(res => res.json())
     .then(data => {
       setQuestions(data.results)
     })
+    }, 2000);
+
+    return () => clearTimeout(timeoutId);
   },[])
   console.log("questions",questions)
   
   const questionSection = questions
-  ? questions.map(oneQuestion => (
+  ? questions.map(oneQuestion => {
+    const correctAnswer = oneQuestion.correct_answer
+    const wrongAnswers = oneQuestion.incorrect_answers
+    const randomIndex = Math.floor(Math.random() * wrongAnswers.length + 1) // Pick a random index between 0 and arr.length as it will be 4 items in it
+    const allQuestionsArr = wrongAnswers.splice(randomIndex,0,correctAnswer) // Insert without removing any elements (deleteCount = 0)
+    console.log(randomIndex)
+    console.log(wrongAnswers)
+    console.log(allQuestionsArr)
+    return (
+    <>
       <h2 key={oneQuestion.question}>{decode(oneQuestion.question)}</h2>
-    ))
+      {/* <input type="radio" defaultChecked={false} name="answer" value={answer} />
+      <label>{answer}</label> */}
+    </>
+)})
   : null
 
   return (
